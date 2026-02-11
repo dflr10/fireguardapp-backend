06eb888cc8293d69902147638ea73dce9cbe0a74
dflr10 <dafelor.10@hotmail.com>
2026-02-10 20:33:22 -0500

    fix(create-emergency.dto): type IsIn string updated

--- src/emergencies/dto/create-emergency.dto.ts
+++ src/emergencies/dto/create-emergency.dto.ts
@@ -20,7 +20,7 @@ export class CreateEmergencyDto {
   stationId: string;
 
   @IsString()
-  @IsIn(["incendio", "accidente", "rescate", "inundacion", "otro"])
+  @IsIn(["flight", "car_crash", "security", "person_remove", "search", "park", "apartment", "bug_report", "waves", "droplet", "water_drop", "plumbing", "terrain", "explosion", "power_off", "notifications_off", "air", "grain", "local_fire_department", "landslide", "forest", "directions_car", "construction", "warning", "helping_hand", "group", "favorite", "record_voice_over", "block", "pets", "wb_sunny", "sensors", "flash_on", "local_hospital", "groups", "heart_plus", "campaign", "flame_delete"])
   type: string;
 
   @IsString()
